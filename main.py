9c86df7b2c3651ee6c3c29710aac96a694fcbe3d
Nuwan Chandima <nuwanchandima71@gmail.com>
2024-12-17 09:12:38 +0530

    update data file path

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ import pandas as pd
 # import os
 # from dotenv import load_dotenv, dotenv_values 
 # print(os.getenv("DATA_SET_PATH"))
-dataFrame=pd.read_csv("/var/www/spera/ML/iris_flower_prediction/data/iris-flower-data.csv")
+dataFrame=pd.read_csv("data//iris-flower-data.csv")
 
 print("Dataframes...\n",dataFrame)
